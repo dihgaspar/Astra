50e920c94838e3275c63c87ccce7a0e45e9c2203
Diogo Gaspar <159595759+dihgaspar@users.noreply.github.com>
2025-09-10 12:01:59 -0300

    Update final1.js

--- js/final1.js
+++ js/final1.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const txtFinal1 = document.getElementById('txtFinal1');
 
         const nome = localStorage.getItem('Nome');
-        txtFinal1.textContent = "Obrigado, " + nome + ", por seguir até aqui comigo! :D";
+        txtFinal1.textContent = "Thanks, " + nome + ", for staying with me until here! :D";
         txtFinal1.style.display = 'block'; // Mostra o texto
+
 })
